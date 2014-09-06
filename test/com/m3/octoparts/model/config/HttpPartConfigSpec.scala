b77258f9165a358f3c1357a6a12d99694349f0de
Lloyd <lloydmeta@gmail.com>
2014-09-06 23:55:51 +0900

    Add check for generated JSON model

--- test/com/m3/octoparts/model/config/HttpPartConfigSpec.scala
+++ test/com/m3/octoparts/model/config/HttpPartConfigSpec.scala
@@ -1,5 +1,8 @@
 package com.m3.octoparts.model.config
 
+import com.m3.octoparts.model.HttpMethod
+import com.m3.octoparts.model.config.json
+import scala.concurrent.duration._
 import com.m3.octoparts.support.mocks.ConfigDataMocks
 import org.scalatest._
 
@@ -17,7 +20,44 @@ class HttpPartConfigSpec extends FunSpec with Matchers with ConfigDataMocks {
     }
 
     it("should not throw when given a config with a HystrixConfig") {
-      val jsonModel = HttpPartConfig.toJsonModel(mockHttpPartConfig.copy(hystrixConfig = Some(mockHystrixConfig)))
+      val jsonModel = HttpPartConfig.toJsonModel(mockHttpPartConfig.copy(
+        hystrixConfig = Some(mockHystrixConfig),
+        additionalValidStatuses = Set(302),
+        cacheGroups = Set(mockCacheGroup)
+      ))
+      val expectedModel = json.HttpPartConfig(
+        partId = "something",
+        owner = "somebody",
+        uriToInterpolate = "http://random.com",
+        description = "",
+        method = HttpMethod.Get,
+        hystrixConfig = json.HystrixConfig(
+          timeoutInMs = 50,
+          threadPoolConfig = json.ThreadPoolConfig(
+            threadPoolKey = "testThreadPool",
+            coreSize = 2,
+            queueSize = 256),
+          commandKey = "command",
+          commandGroupKey = "GroupKey"),
+        additionalValidStatuses = Set(302),
+        parameters = Set(
+          json.PartParam(
+            required = true,
+            versioned = false,
+            paramType = ParamType.Header,
+            outputName = "userId",
+            inputNameOverride = None,
+            cacheGroups = Set()
+          )),
+        deprecatedInFavourOf = None,
+        cacheGroups = Set(mockCacheGroup).map(CacheGroup.toJsonModel),
+        cacheTtl = Some(60 seconds),
+        alertMailsEnabled = true,
+        alertAbsoluteThreshold = Some(1000),
+        alertPercentThreshold = Some(33.0),
+        alertInterval = 10 minutes,
+        alertMailRecipients = Some("l-chan@m3.com"))
+      jsonModel should be(expectedModel)
     }
 
   }
