499d334975a3d03f2e93217c73f614fbbc928dba
Lloyd <lloydmeta@gmail.com>
2014-09-16 16:03:41 +0900

    instrumentSettings needs to come first

--- project/Build.scala
+++ project/Build.scala
@@ -166,11 +166,12 @@ object OctopartsBuild extends Build {
   }
 
   lazy val scoverageSettings =
+    instrumentSettings ++
     Seq(
       ScoverageKeys.highlighting := true,
       ScoverageKeys.excludedPackages in ScoverageCompile := """com\.kenshoo.*;.*controllers\.javascript\..*;.*controllers\.ref\..*;.*controllers\.Reverse.*;.*BuildInfo.*;.*views\.html\..*;Routes""",
       testOptions in ScoverageTest += Tests.Argument("-u", "target/test-reports")
-    ) ++ instrumentSettings
+    )
 
   lazy val formatterPrefs = Seq(
     ScalariformKeys.preferences := ScalariformKeys.preferences.value
