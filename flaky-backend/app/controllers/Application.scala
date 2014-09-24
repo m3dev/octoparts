a31190daf5572c7eeea7c4e398c112a195109c97
Chris Birchall <c-birchall@m3.com>
2014-09-24 12:43:23 +0900

    Add a couple more useful endpoints to the test backend

--- flaky-backend/app/controllers/Application.scala
+++ flaky-backend/app/controllers/Application.scala
@@ -1,10 +1,15 @@
 package controllers
 
-import play.api.libs.concurrent.Promise
+import play.api._
 import play.api.mvc._
+import play.api.libs.concurrent.Promise
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Application extends Controller {
-  import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+  def fast = Action{
+    Ok("Quick as a flash!")
+  }
 
   def slow(delay: Int) = Action.async {
     Promise.timeout(Ok("Sorry for the wait!"), delay)
@@ -15,6 +20,24 @@ object Application extends Controller {
       Ok("No problem!")
     else
       InternalServerError("Whoops!")
+  }
+
+  /**
+   * Emulates a backend with ETag support and/or custom Cache-Control header.
+   *
+   * If passed an `etag` query param, 
+   * - if request has an `If-None-Match` header, and it matches the query param, returns a 304.
+   * - otherwise, return a 200 with an `ETag` header attached.
+   *
+   * If passed a `cacheControl` query param, attaches it to the 200 response.
+   */
+  def cacheHeaders(etag: String, cacheControl: Option[String]) = Action { request =>
+    if (request.headers.get(IF_NONE_MATCH) == Some(etag)) {
+      NotModified
+    } else {
+      val result = Ok(s"ETag: $etag, cache-control: $cacheControl").withHeaders(ETAG -> etag)
+      cacheControl.fold[Result](result)(cc => result.withHeaders(CACHE_CONTROL -> cc))
+    }
   }
 
 }
