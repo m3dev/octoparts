5f99d9ae910da9a2ce3276c6f464c255d9e7994e
Lloyd <lloydmeta@gmail.com>
2014-09-24 22:41:47 +0900

    Add check for addhostname

--- test/com/m3/octoparts/logging/LTSVLogWriterSpec.scala
+++ test/com/m3/octoparts/logging/LTSVLogWriterSpec.scala
@@ -1,9 +1,11 @@
 package com.m3.octoparts.logging
 
+import org.mockito.ArgumentCaptor
 import org.scalatest.{ Matchers, FunSpec }
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
 import org.mockito.Matchers._
+import org.slf4j.{ LoggerFactory, Logger }
 import play.api.LoggerLike
 
 class LTSVLogWriterSpec extends FunSpec with MockitoSugar with Matchers {
@@ -16,6 +18,36 @@ class LTSVLogWriterSpec extends FunSpec with MockitoSugar with Matchers {
       val logger: LoggerLike = loggerLike
     }
     (writer, loggerLike)
+  }
+
+  def writerWithDebugCapture: (LTSVLogWriter, Function0[String]) = {
+    var debugMessage: String = ""
+    val loggerLike = new LoggerLike {
+      override val logger: Logger = LoggerFactory.getLogger("application")
+      override def debug(message: => String): Unit = {
+        debugMessage = message
+      }
+    }
+    val writer = new LTSVLogWriter {
+      val logger: LoggerLike = loggerLike
+    }
+    (writer, () => debugMessage)
+  }
+
+  describe("logging with hostnames") {
+    it("should send a message with 'octopartsHost' to the underlying logger") {
+      val (writer, captureDebugMsg) = writerWithDebugCapture
+      writer.debug(true, "hi" -> "there")
+      captureDebugMsg() contains ("octopartsHost") should be(true)
+    }
+  }
+
+  describe("logging without hostnames") {
+    it("should send a message without 'octopartsHost' to the underlying logger") {
+      val (writer, captureDebugMsg) = writerWithDebugCapture
+      writer.debug(false, "hi" -> "there")
+      captureDebugMsg() contains ("octopartsHost") should be(false)
+    }
   }
 
   describe("#debug and not passing a Throwable") {
