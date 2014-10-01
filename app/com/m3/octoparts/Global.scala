5153fef1ae165fe6962fbd4477f9dd88866aa8a8
Lloyd <lloydmeta@gmail.com>
2014-10-01 22:37:41 +0900

    Check and try to set Properties Strategy in Global
    Might be a good idea to try to make sure CachelessHystrixPropertiesStrategy is used in Global
    during onStart if the system property is not set

--- app/com/m3/octoparts/Global.scala
+++ app/com/m3/octoparts/Global.scala
@@ -7,10 +7,11 @@ import _root_.controllers.ControllersModule
 import com.kenshoo.play.metrics.MetricsFilter
 import com.m3.octoparts.cache.CacheModule
 import com.m3.octoparts.http.HttpModule
-import com.m3.octoparts.hystrix.{ HystrixMetricsLogger, HystrixModule }
+import com.m3.octoparts.hystrix.{ CachelessHystrixPropertiesStrategy, HystrixMetricsLogger, HystrixModule }
 import com.m3.octoparts.logging.PartRequestLogger
 import com.beachape.logging.LTSVLogger
 import com.m3.octoparts.repository.RepositoriesModule
+import com.netflix.hystrix.strategy.HystrixPlugins
 import com.typesafe.config.ConfigFactory
 import com.wordnik.swagger.config.{ ConfigFactory => SwaggerConfigFactory }
 import com.wordnik.swagger.model.ApiInfo
@@ -22,6 +23,7 @@ import scaldi.play.ScaldiSupport
 
 import scala.collection.concurrent.TrieMap
 import scala.concurrent.duration._
+import scala.util.control.NonFatal
 
 object Global extends WithFilters(MetricsFilter) with ScaldiSupport {
 
@@ -80,6 +82,7 @@ object Global extends WithFilters(MetricsFilter) with ScaldiSupport {
   }
 
   override def onStart(app: Application) = {
+    setHystrixPropertiesStrategy(app)
     super.onStart(app)
     startPeriodicTasks(app)
   }
@@ -94,6 +97,31 @@ object Global extends WithFilters(MetricsFilter) with ScaldiSupport {
     val hystrixLoggingInterval = app.configuration.underlying.getDuration("hystrix.logging.intervalMs", TimeUnit.MILLISECONDS).toInt.millis
     Akka.system.scheduler.schedule(hystrixLoggingInterval, hystrixLoggingInterval) {
       HystrixMetricsLogger.logHystrixMetrics()
+    }
+  }
+
+  /**
+   * Tries to set the Hystrix properties strategy to CachelessHystrixPropertiesStrategy
+   *
+   * Resist the temptation to do a HystrixPlugins.getInstance().getPropertiesStrategy first to do
+   * checking, as that actually also sets the strategy if it isn't already set.
+   */
+  def setHystrixPropertiesStrategy(app: Application): Unit = {
+    // If it's defined, we don't need to set anything
+    if (sys.props.get("hystrix.plugin.HystrixPropertiesStrategy.implementation").isEmpty) {
+      LTSVLogger.warn("-Dhystrix.plugin.HystrixPropertiesStrategy.implementation is not set. It should be" -> "com.m3.octoparts.hystrix.CachelessHystrixPropertiesStrategy")
+      try {
+        HystrixPlugins.getInstance().registerPropertiesStrategy(new CachelessHystrixPropertiesStrategy)
+      } catch {
+        case NonFatal(e) => {
+          val currentStrategy = HystrixPlugins.getInstance().getPropertiesStrategy.getClass
+          if (currentStrategy != classOf[CachelessHystrixPropertiesStrategy]) {
+            LTSVLogger.error(e, "Current Hystrix Properties Strategy:" -> currentStrategy)
+            if (Play.mode(app) != Mode.Test)
+              sys.error("Tried but failed to set CachelessHystrixPropertiesStrategy as HystrixPropertiesStrategy")
+          }
+        }
+      }
     }
   }
 }
