9750503f78ef1171ac2eba8d0501b3c87687faae
Vincent PERICART <v-pericart@m3.com>
2014-10-06 12:18:35 +0900

    store again if entry has expired locally but not remotely.

--- app/com/m3/octoparts/cache/memcached/MemoryBufferingRawCache.scala
+++ app/com/m3/octoparts/cache/memcached/MemoryBufferingRawCache.scala
@@ -2,7 +2,7 @@ package com.m3.octoparts.cache.memcached
 
 import java.util.concurrent.TimeUnit
 
-import com.google.common.cache.{CacheBuilder, Cache => GuavaCache}
+import com.google.common.cache.{ CacheBuilder, Cache => GuavaCache }
 import com.m3.octoparts.cache.RawCache
 import play.api.Logger
 import shade.memcached.Codec
@@ -10,6 +10,7 @@ import skinny.util.LTSV
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
 
 class MemoryBufferingRawCache(networkCache: RawCache, localCacheDuration: Duration) extends RawCache {
 
@@ -18,31 +19,42 @@ class MemoryBufferingRawCache(networkCache: RawCache, localCacheDuration: Durati
       .expireAfterWrite(localCacheDuration.toMillis, TimeUnit.MILLISECONDS)
       .build[String, Object]()
 
-  override def set[T](key: String, value: T, exp: Duration)(implicit codec: Codec[T]): Future[Unit] = {
+  def set[T](key: String, value: T, exp: Duration)(implicit codec: Codec[T]): Future[Unit] = {
     if (exp >= localCacheDuration) {
-      memoryCache.put(key, value.asInstanceOf[Object])
-      if (Logger.isTraceEnabled) {
-        Logger.trace(LTSV.dump("Key set in local cache" -> key, "for" -> localCacheDuration.toString))
-      }
+      storeInMemoryCache(key, value)
     }
     networkCache.set(key, value, exp)
   }
 
-  override def get[T](key: String)(implicit codec: Codec[T]): Future[Option[T]] = {
+  def get[T](key: String)(implicit codec: Codec[T]): Future[Option[T]] = {
     val local = Option(memoryCache.getIfPresent(key))
     local match {
-      case None =>
+      case Some(value) => Future.successful(Some(value.asInstanceOf[T]))
+      case _ =>
         if (Logger.isTraceEnabled) {
           Logger.trace(LTSV.dump("Key missing in local cache" -> key))
         }
-        networkCache.get(key)
-      case Some(obj) => Future.successful(Some(obj.asInstanceOf[T]))
+        val cachePoll = networkCache.get(key)
+        cachePoll.onSuccess {
+          case Some(value) => storeInMemoryCache(key, value)
+        }(ExecutionContext.global)
+        cachePoll
     }
   }
 
-  override def close(): Unit = {
+  def close(): Unit = {
     memoryCache.asMap().clear()
     networkCache.close()
   }
 
+  private def storeInMemoryCache(key: String, value: Any): Unit = {
+    value match {
+      case obj: Object =>
+        memoryCache.put(key, obj)
+        if (Logger.isTraceEnabled) {
+          Logger.trace(LTSV.dump("Key set in local cache" -> key, "to" -> obj.toString, "for" -> localCacheDuration.toString))
+        }
+      case _ =>
+    }
+  }
 }
