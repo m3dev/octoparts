479dde6dcdae0a32a4abe877053b42695e73e87e
Vincent PERICART <v-pericart@m3.com>
2015-01-27 11:17:42 +0900

    Rework the AggregateResponse enrichments

--- scala-ws-client/src/test/scala/com/m3/octoparts/ws/Sample.scala
+++ scala-ws-client/src/test/scala/com/m3/octoparts/ws/Sample.scala
@@ -28,36 +28,41 @@ object Sample {
   // Create a client
   val octoClient = new OctoClient("http://octoparts/", clientTimeout = 1.second)
 
-  // Build an AggregateRequest to send to Octoparts
-  val aggregateRequest = AggregateRequest(
-    requestMeta = RequestMeta(
+  implicit object rmb extends RequestMetaBuilder[Int] {
+    def apply(userId: Int) = RequestMeta(
       // Most of these fields are optional
       id = UUID.randomUUID().toString,
       serviceId = Some("frontend"),
-      userId = Some("123"),
+      userId = Some(userId.toString),
       requestUrl = Some("http://www.m3.com/foo"),
       timeout = Some(500.millis)
-    ),
-    requests = Seq(
-      // A list of the endpoints you want to call, with parameters to send
-      PartRequest(partId = "UserProfile", params = Seq(PartRequestParam("uid", "123"))),
-      PartRequest(partId = "LatestNews", params = Seq(PartRequestParam("limit", "10")))
     )
+  }
+
+  // Prepare part requests to send to Octoparts
+  val requests = Seq(
+    // A list of the endpoints you want to call, with parameters to send
+    PartRequest(partId = "UserProfile", params = Seq(PartRequestParam("uid", "123"))),
+    PartRequest(partId = "LatestNews", params = Seq(PartRequestParam("limit", "10")))
   )
 
-  // Send the request, get back a Future
-  val fResp: Future[AggregateResponse] = octoClient.invoke(aggregateRequest)
+  // Send the requests, get back a Future
+  val fResp: Future[AggregateResponse] = octoClient.invoke(123, requests)
 
   fResp.map { aggregateResponse =>
 
     // Result will be None if:
     // - Octoparts did not return a response for this endpoint (e.g. because the endpoint was too slow to respond)
+    // - the response contained errors
     // - the response could not be deserialized into a UserProfile
     val userProfile: Option[UserProfile] = aggregateResponse.getJsonPart[UserProfile]("UserProfile")
 
     // Get the list of latest news articles, falling back to an empty list in case of error
     val latestNews: Seq[NewsArticle] = aggregateResponse.getJsonPartOrElse[Seq[NewsArticle]]("LatestNews", default = Nil)
 
+    // Get the list of latest news article, falling back to another json model in case of error
+    val userNews: Either[Option[UserProfile], NewsArticle] = aggregateResponse.getJsonPartOrError[NewsArticle, UserProfile]("UserNews")
+    userNews.right.toOption == aggregateResponse.getJsonPart[NewsArticle]("UserNews")
     // Do stuff with the user profile and the list of news articles ...
 
   }
