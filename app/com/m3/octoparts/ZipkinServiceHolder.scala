f89d3a096754c22d5f9cd5586801ff2203d9bd83
Lloyd <lloydmeta@gmail.com>
2015-03-05 16:25:42 +0900

    Add support for random sampling

--- app/com/m3/octoparts/ZipkinServiceHolder.scala
+++ app/com/m3/octoparts/ZipkinServiceHolder.scala
@@ -6,6 +6,8 @@ import com.beachape.zipkin.services.{ BraveZipkinService, NoopZipkinService, Zip
 import com.github.kristofa.brave.zipkin.ZipkinSpanCollector
 import play.api.{ Logger, Play }
 
+import scala.util.{ Failure, Success, Try }
+
 object ZipkinServiceHolder {
 
   private implicit val app = play.api.Play.current
@@ -17,25 +19,41 @@ object ZipkinServiceHolder {
     val maybeService = for {
       zipkinHost <- Play.configuration.getString("zipkin.host")
       zipkinPort <- Play.configuration.getInt("zipkin.port")
+      zipkinRate <- Play.configuration.getDouble("zipkin.sampleRate")
       env <- Play.configuration.getString("application.env")
     } yield {
-      val zipkinSpanCollector = new ZipkinSpanCollector(zipkinHost, zipkinPort)
-      sys.addShutdownHook(zipkinSpanCollector.close())
-      val currentHostName = InetAddress.getLocalHost.getHostName
-      val currentRunningPort = Play.configuration.getInt("http.port").getOrElse(9000)
-      new BraveZipkinService(
-        currentHostName,
-        currentRunningPort, s"Octoparts - $env",
-        zipkinSpanCollector,
-        Seq(
-          !_.startsWith("OPTION"),
-          !_.startsWith("GET - /assets")
+      Try {
+        val zipkinSpanCollector = new ZipkinSpanCollector(zipkinHost, zipkinPort)
+        sys.addShutdownHook(zipkinSpanCollector.close())
+        val currentHostName = InetAddress.getLocalHost.getHostName
+        val currentRunningPort = Play.configuration.getInt("http.port").getOrElse(9000)
+        new BraveZipkinService(
+          hostIp = currentHostName,
+          hostPort = currentRunningPort,
+          serviceName = s"Octoparts - $env",
+          collector = zipkinSpanCollector,
+          clientTraceFilters = Seq(_.isSetParent_id),
+          serverTraceFilters = Seq(
+            { s =>
+              val name = s.getName
+              !(name.startsWith("OPTION") || name.startsWith("GET - /assets"))
+            },
+            { s => s.isSetParent_id || (zipkinRate >= scala.util.Random.nextDouble()) }
+          )
         )
-      )
+      }
+
     }
-    maybeService.getOrElse {
-      Logger.warn("Zipkin configs are missing in the current environment, falling back to NoopZipkinService")
-      NoopZipkinService
+    maybeService match {
+      case Some(Success(zipkinService)) => zipkinService
+      case Some(Failure(e)) => {
+        Logger.error("Could not create the Zipkin service", e)
+        NoopZipkinService
+      }
+      case None => {
+        Logger.warn("Zipkin configs are missing in the current environment, falling back to NoopZipkinService")
+        NoopZipkinService
+      }
     }
   }
 
