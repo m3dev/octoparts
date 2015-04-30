82ffbf1c8b77636d961b978216bee5ec6b01f68b
Vincent PERICART <v-pericart@m3.com>
2015-04-30 10:46:54 +0900

    Upgrade minor scala version number in publish script

--- scripts/publish_libs.sh
+++ scripts/publish_libs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 sbt \
-++2.10.4 \
+++2.10.5 \
 clean \
 auth-plugin-api/publishSigned \
 java-client/publishSigned \
 models/publishSigned \
 play-json-formats/publishSigned \
 scala-ws-client/publishSigned \
-++2.11.5 \
+++2.11.6 \
 clean \
 auth-plugin-api/publishSigned \
 java-client/publishSigned \
