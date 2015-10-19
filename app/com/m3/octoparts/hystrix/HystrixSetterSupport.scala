f93602c6592e4b3b71c89cc122ab0ddbc4cf5dcf
Lloyd <lloydmeta@gmail.com>
2015-10-19 12:22:27 +0900

    Make thread pool setter configuration transform testable and test it

--- app/com/m3/octoparts/hystrix/HystrixSetterSupport.scala
+++ app/com/m3/octoparts/hystrix/HystrixSetterSupport.scala
@@ -1,6 +1,7 @@
 package com.m3.octoparts.hystrix
 
-import com.m3.octoparts.model.config.HystrixConfig
+import com.m3.octoparts.model.config.{ ThreadPoolConfig, HystrixConfig }
+import com.netflix.hystrix.HystrixThreadPoolProperties.Setter
 import com.netflix.hystrix._
 
 /**
@@ -13,12 +14,8 @@ trait HystrixSetterSupport {
    * Given a HystrixArgument case class returns a HystrixCommand.Setter object
    */
   def setter(config: HystrixConfig): HystrixCommand.Setter = {
-    val threadPoolConfig = config.threadPoolConfigItem
-    val threadPoolProperties = HystrixThreadPoolProperties.Setter().
-      withCoreSize(threadPoolConfig.coreSize).
-      // Hystrix uses both of these for setting Queue size
-      withMaxQueueSize(threadPoolConfig.queueSize).
-      withQueueSizeRejectionThreshold(threadPoolConfig.queueSize)
+    val threadPoolConfig: ThreadPoolConfig = config.threadPoolConfigItem
+    val threadPoolProperties = threadPoolSetter(threadPoolConfig)
 
     HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(config.commandGroupKey)).
       andCommandKey(HystrixCommandKey.Factory.asKey(config.commandKey)).
@@ -27,5 +24,13 @@ trait HystrixSetterSupport {
       andCommandPropertiesDefaults(HystrixCommandProperties.Setter().
         withExecutionTimeoutInMilliseconds(config.timeout.toMillis.toInt).
         withFallbackEnabled(config.localContentsAsFallback))
+  }
+
+  private[hystrix] def threadPoolSetter(threadPoolConfig: ThreadPoolConfig): Setter = {
+    HystrixThreadPoolProperties.Setter().
+      withCoreSize(threadPoolConfig.coreSize).
+      // Hystrix uses both of these for setting Queue size
+      withMaxQueueSize(threadPoolConfig.queueSize).
+      withQueueSizeRejectionThreshold(threadPoolConfig.queueSize)
   }
 }
