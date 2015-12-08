61e4b385303fcbb33e4a5def4faf51c35de69b95
lloydmeta <lloydmeta@gmail.com>
2015-12-08 18:00:30 +0900

    Update publish_libs.sh

--- scripts/publish_libs.sh
+++ scripts/publish_libs.sh
@@ -3,14 +3,14 @@
 sbt \
 ++2.10.5 \
 clean \
-auth-plugin-api/publishSigned \
+auth-handler-api/publishSigned \
 java-client/publishSigned \
 models/publishSigned \
 play-json-formats/publishSigned \
 scala-ws-client/publishSigned \
-++2.11.6 \
+++2.11.7 \
 clean \
-auth-plugin-api/publishSigned \
+auth-handler-api/publishSigned \
 java-client/publishSigned \
 models/publishSigned \
 play-json-formats/publishSigned \
